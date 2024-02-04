42b8b80227c95b1b0d58322b07976959d55c48f4
Egor Krasovsky <e.krasovsky@raketa.travel>
2024-02-04 23:44:45 +0300

    Second Homework

---
+++ src/com/ekdev/homeworks/secondHomework/loopsHomework/FirstExercise.java
@@ -0,0 +1,35 @@
+package com.ekdev.homeworks.secondHomework.loopsHomework;
+
+public class FirstExercise {
+
+    public static void main(String[] args) {
+        System.out.println(getAmountOfEvenNumbers(1234));
+        System.out.println(getAmountOfOddNumbers(3789));
+        System.out.println(getAmountOfEvenNumbers(-1234));
+        System.out.println(getAmountOfOddNumbers(-334455));
+    }
+
+    private static int getAmountOfEvenNumbers(int numberToCheck) {
+        int amount = 0;
+        while (numberToCheck != 0) {
+            int remainder = numberToCheck % 10;
+            if (remainder % 2 == 0) {
+                amount++;
+            }
+            numberToCheck /= 10;
+        }
+        return amount;
+    }
+
+    private static int getAmountOfOddNumbers(int numberToCheck) {
+        int amount = 0;
+        while (numberToCheck != 0) {
+            int remainder = numberToCheck % 10;
+            if (remainder % 2 != 0) {
+                amount++;
+            }
+            numberToCheck /= 10;
+        }
+        return amount;
+    }
+}
---
+++ src/com/ekdev/homeworks/secondHomework/loopsHomework/ThirdExercise.java
@@ -0,0 +1,10 @@
+package com.ekdev.homeworks.secondHomework.loopsHomework;
+
+public class ThirdExercise {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
