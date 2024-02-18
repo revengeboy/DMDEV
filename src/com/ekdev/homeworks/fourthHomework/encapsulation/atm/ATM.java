fa5e9917434c02338f81d8bc310e93f210ca6861
Egor Krasovsky <e.krasovsky@raketa.travel>
2024-02-19 00:05:30 +0300

    fix first exercise

--- src/com/ekdev/homeworks/fourthHomework/encapsulation/atm/ATM.java
+++ src/com/ekdev/homeworks/fourthHomework/encapsulation/atm/ATM.java
@@ -28,27 +28,39 @@ public class ATM {
         int i = amount % 50;
         if (i == 0 && amount < getFiftyBanknoteSum()) {
             System.out.println("Банкомат выдал купюры по 50 количеством: " + amount / 50);
+            setFiftyBanknoteAmount(getFiftyBanknoteAmount() - amount / 50);
         } else if (amount > getFiftyBanknoteSum()) {
             int amountAfterFifty = amount - getFiftyBanknoteSum();
-            System.out.println("Банкомат выдал все купюры по 50 количеством: " + amount / 50);
+            System.out.println("Банкомат выдал все купюры по 50 количеством: " + getFiftyBanknoteAmount());
+            setFiftyBanknoteAmount(0);
             int y = amountAfterFifty % 20;
             if (y == 0 && amountAfterFifty < getTwentyBanknoteSum()) {
-                System.out.println("Банкомат выдал купюры по 20 количеством: " + amount / 50);
+                System.out.println("Банкомат выдал купюры по 20 количеством: " + amount / 20);
+                setTwentyBanknoteAmount(getTwentyBanknoteAmount() - amount / 20);
             } else if (amountAfterFifty > getTwentyBanknoteSum()) {
                 int amountAfterTwenty = amountAfterFifty - getTwentyBanknoteSum();
-                System.out.println("Банкомат выдал все купюры по 20 количеством: " + amount / 50);
+                System.out.println("Банкомат выдал все купюры по 20 количеством: " + getTenBanknoteAmount());
+                setTwentyBanknoteAmount(0);
                 int z = amountAfterTwenty % 10;
                 if (z == 0 && amountAfterTwenty < getTenBanknoteSum()) {
                     System.out.println("Банкомат выдал купюры по 10 количеством: " + i / 10);
+                    setTenBanknoteAmount(getTenBanknoteAmount() - amount / 10);
                 }
             }
         }
     }
 
     private boolean isEnoughMoneyInATM(int amount) {
-        int moneyInATM = calculateAllMoneyInATM();
-        System.out.println("Сумма в банкомате: " + moneyInATM);
-        return amount < moneyInATM;
+        int allMoneyInATM = calculateAllMoneyInATM();
+        printInfoAboutMoneyInATM(allMoneyInATM);
+        return amount < allMoneyInATM;
+    }
+
+    private void printInfoAboutMoneyInATM(int allMoney) {
+        System.out.println("Сумма в банкомате: " + allMoney);
+        System.out.println("Количество купюр по 50: " + getFiftyBanknoteAmount());
+        System.out.println("Количество купюр по 20: " + getTwentyBanknoteAmount());
+        System.out.println("Количество купюр по 10: " + getTenBanknoteAmount());
     }
 
     private int calculateAllMoneyInATM() {
